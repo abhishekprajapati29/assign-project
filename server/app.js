6b6165155f4f6e64947d26c936d75e5096b16554
Abhishek Prajapati <45328242+abhishekprajapati29@users.noreply.github.com>
2024-02-19 14:02:48 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require("morgan");
 var cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 app.use(cors());
 
 require("dotenv").config();
