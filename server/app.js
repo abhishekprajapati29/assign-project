d7d1d0d64fc1b164eb501ef1aec578632ec9479b
Abhishek Prajapati <45328242+abhishekprajapati29@users.noreply.github.com>
2024-02-19 14:11:27 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -58,10 +58,10 @@ app.use((req, res, next) => {
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/build")));
+  app.use(express.static(path.join(__dirname1, "client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
