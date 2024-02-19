65350fe9101fb815e6691af8ca0ef3af6e76c63d
Abhishek Prajapati <45328242+abhishekprajapati29@users.noreply.github.com>
2024-02-19 14:30:06 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -60,12 +60,12 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "client/build")));
 
-  app.get("*", (req, res) =>
+  app.get("*", (req, res) => {
     console.log(path.resolve(__dirname1, "client", "build", "index.html"), path.join(__dirname1, "client/build"));
     if (req.get('host') === "www.assign-project.onrender.com") {
       res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
     }
-  );
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
