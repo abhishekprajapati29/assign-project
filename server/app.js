748090db125a48ad850ace23a84eafb52f86fdd6
Abhishek Prajapati <45328242+abhishekprajapati29@users.noreply.github.com>
2024-02-19 14:25:09 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -61,7 +61,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+    console.log(path.resolve(__dirname1, "client", "build", "index.html"), path.join(__dirname1, "client/build"));
+    if (req.get('host') === "assign-project.onrender.com") {
+      res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+    }
   );
 } else {
   app.get("/", (req, res) => {
